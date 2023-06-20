5ad61d161df722df936bc75213bff19176401f5d
Rossi21 <rossi21@gmail.com>
2023-06-20 06:43:11 -0700

    books_page

--- users_app/urls.py
+++ users_app/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
 
 path('', views.index),
-path('users',views.users),
+path('books/<int:id>', views.show_book),
+path('users', views.users),
+path('users/<int:id>', views.show_user),
 path('create/user', views.create_user),
 path('create/book', views.create_book),
 
